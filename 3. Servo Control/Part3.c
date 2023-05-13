dfe89147bbd4beaf7adb38d0adc4cd9eb6a7c93e
Jay Gianduso <jaygia11@gmail.com>
2023-05-12 23:47:15 -0400

    fixed part 3

--- 3. Servo Control/Part3.c
+++ 3. Servo Control/Part3.c
@@ -5,162 +5,101 @@
  *      Author: Jay Gianduso
  *
  */
+
 #include <msp430.h>
 
+void gpioInit(){
 
-char led_state = 0;
+    // Servo pin setup
+    P2DIR |= BIT0;      // P2.0 set as output
+    P2OUT &= ~BIT0;     // Clear register
+    P2SEL0 |= BIT0;     // Set peripheral function
+    P2SEL1 &= ~BIT0;    // Set peripheral function
 
-int red_duty = 0;
-int green_duty = 999;
-int blue_duty = 999;
+    // Button 2.3 setup
+    P2DIR &= ~BIT3;     // P2.3 set as input
+    P2OUT |= BIT3;      // P2.3 set to 1
+    P2REN |= BIT3;      // enable pull up
+    P2IES &= ~BIT3;     // Low --> High edge
+    P2IE |= BIT3;       // interrupt enabled
 
-
-void led_setup()
-{
-
-    // Configure output pins for the LEDs
-    P6DIR |= BIT0 | BIT1 | BIT2;    // P6.0, 6.1, 6.2 set as output
-    P6OUT &= ~(BIT0 | BIT1 | BIT2); // Clear register
-    P6SEL0 |= BIT0 | BIT1 | BIT2;   // Select peripheral function
-    P6SEL1 &= ~(BIT0 | BIT1 | BIT2);
-    P6IE |= BIT0 | BIT1 | BIT2;     // Enable interrupt
-
-}
-
-void timer_B1_setup()
-{
-
-    // Initialize Timer B1 for PWM output
-    TB1CCR0 = 1000 - 1;
-    TB1CTL = TBSSEL__SMCLK | MC__UP | TBCLR;
-    TB1CCTL1 = OUTMOD_7;
-    TB1CCTL2 = OUTMOD_7;
-    TB1CCTL3 = OUTMOD_7;
-
-    // Set initial duty cycles for the LEDs
-    TB1CCR1 = red_duty;
-    TB1CCR2 = green_duty;
-    TB1CCR3 = blue_duty;
+    // Button 4.1 setup
+    P4DIR &= ~BIT1;     // P4.1 set to input
+    P4OUT |= BIT1;      // P4.1 set to 1
+    P4REN |= BIT1;      // enable pull-up
+    P4IES &= ~BIT1;     // Low --> High edge
+    P4IE |= BIT1;       // interrupt enabled
 
 }
 
-void timer_B0_setup()
-{
 
-    // Initialize Timer B0 for LED color cycling
-    TB0CCTL0 = CCIE;
-    TB0CCR0 = 1;
-    TB0CTL = TBSSEL_1 | MC_2 | ID_3 | TBCLR | TBIE;
+void TimerB1Setup(){
+
+    // Set up TB3
+    TB1CTL = TBSSEL_2 | MC__UP | TBCLR | TBIE;    // SMCLK | up mode | clear TBR | interrupt enabled
+    TB1CCTL0 = OUTMOD_7;                          // CCR1 reset/set
+    TB1CCTL1 |= CCIE;                             // Enable Interrupt
+    TB1CCR0 = 20000;                              // set frequency to 50Hz
+    TB1CCR1 = 1500;                               // set duty cycle to 15%
 
 }
+
 
 int main(void)
 {
 
-    WDTCTL = WDTPW | WDTHOLD;   // Disable watchdog timer
+    WDTCTL = WDTPW | WDTHOLD;   // Stop WDT
 
 
-    led_setup();
-    timer_B1_setup();
-    timer_B0_setup();
+    gpioInit();
+    TimerB1Setup();
 
-    PM5CTL0 &= ~LOCKLPM5;
+    PM5CTL0 &= ~LOCKLPM5;   // Disable the GPIO power-on default high-impedance mode
 
-    __bis_SR_register(LPM0_bits | GIE); // Enable low power mode
-
-    __no_operation();   // Do nothing
+    __bis_SR_register(LPM0_bits + GIE);          // Enter LPM0 w/ interrupt
 
 }
 
-
-
-
-
-
-
-#pragma vector = TIMER0_B0_VECTOR
-__interrupt void Timer0_B0_ISR(void)
+// Left button interrupt
+#pragma vector=PORT4_VECTOR
+__interrupt void Port_4(void)
 {
-    // Update the duty cycles for the LEDs based on the current state
-    switch(led_state)
-    {
-        case 0:  // Red to orange
-            red_duty = 0;
-            blue_duty = 999;
-            green_duty--;
-            if(green_duty == 0)
-                led_state = 1;
-            break;
 
-        case 1: // Orange to green
-            blue_duty = 999;
-            green_duty = 0;
-            red_duty++;
-            if(red_duty == 999)
-                led_state = 2;
-            break;
+    P4IFG &= ~BIT1;         // Clear P4.1 interrupt flag
 
-        case 2: // Green to cyan
-            red_duty = 999;
-            green_duty = 0;
-            blue_duty--;
-            if(blue_duty == 0)
-                led_state = 3;
-            break;
+    if(TB1CCR1 < 2000)      // Set maximum position to 5% duty cycle
+        TB1CCR1 += 100;     // Increase the duty cycle by 1% to move the servo right
+    else
+        TB1CCR1 = TB1CCR1;  // do nothing if at maximum position
 
-        case 3: // Cyan to blue
-            red_duty = 999;
-            blue_duty = 0;
-            green_duty++;
-            if(green_duty == 999)
-                led_state = 0;
-            break;
-
-        default:
-            red = 0;                    // Red duty cycle is at 0%
-            green = 999;                // Green duty cycle is at 100%
-            blue = 999;                 // Blue duty cycle is at 100%
-            LEDstate = 0;               // Move to first case
-    }
 }
 
-#pragma vector = PORT6_VECTOR
-__interrupt void Port6_ISR(void)
+// Right button interrupt
+#pragma vector=PORT2_VECTOR
+__interrupt void Port_2(void)
 {
-    if (P6IFG & BIT0) // If interrupt flag is set for P6.0 (RED LED)
+
+    P2IFG &= ~BIT3;         // Clear P2.3 interrupt flag
+
+    if(TB1CCR1 > 1000)      // set minimum position at 5% duty cycle
+        TB1CCR1 -= 100;     // Decrease the duty cycle by 1% to move the servo left
+    else
+        TB1CCR1 = TB1CCR1;  // do nothing if at minimum position
+
+}
+
+// TimerB1 interrupt
+#pragma vector = TIMER3_B1_VECTOR
+__interrupt void Timer3_B1_ISR(void)
+{
+
+    switch(__even_in_range(TB1IV,TB1IV_TBIFG))
     {
-        P6IFG &= ~BIT0; // Clear interrupt flag
-        if (TB1CCR1 == 0) // If the RED LED is off
-        {
-            TB1CCR1 = 1000 - 1; // Turn on RED LED
-        }
-        else // If the RED LED is on
-        {
-            TB1CCR1 = 0; // Turn off RED LED
-        }
+        case  TB1IV_TBCCR1:     // Rising edge interrupt
+            P2OUT &= ~BIT0;     // Clear pin 2.0
+            break;
+        default:                // Falling edge interrupt
+            break;
     }
-    else if (P6IFG & BIT1) // If interrupt flag is set for P6.1 (GREEN LED)
-    {
-        P6IFG &= ~BIT1; // Clear interrupt flag
-        if (TB1CCR2 == 0) // If the GREEN LED is off
-        {
-            TB1CCR2 = 1000 - 1; // Turn on GREEN LED
-        }
-        else // If the GREEN LED is on
-        {
-            TB1CCR2 = 0; // Turn off GREEN LED
-        }
-    }
-    else if (P6IFG & BIT2) // If interrupt flag is set for P6.2 (BLUE LED)
-    {
-        P6IFG &= ~BIT2; // Clear interrupt flag
-        if (TB1CCR3 == 0) // If the BLUE LED is off
-        {
-            TB1CCR3 = 1000 - 1; // Turn on BLUE LED
-        }
-        else // If the BLUE LED is on
-        {
-            TB1CCR3 = 0; // Turn off BLUE LED
-        }
-    }
+
 }
